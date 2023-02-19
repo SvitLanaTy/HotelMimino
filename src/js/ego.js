2064d27a2876039311cf8f3ecdcfd2aa79771c11
Seven <graf.lancelot7@gmail.com>
2023-02-19 19:43:08 +0200

    js

---
+++ src/js/ego.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper('.about-right', {
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  centeredSlides: true,
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      centeredSlides: false,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
