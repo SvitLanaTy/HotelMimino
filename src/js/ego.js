fbaa741fee257a38f9a81497c19e65b931ef6e82
Seven <graf.lancelot7@gmail.com>
2023-02-20 17:17:26 +0200

    add slider

--- src/js/ego.js
+++ src/js/ego.js
@@ -43,6 +43,29 @@ var swiperAboutLeft = new Swiper('.swiper-about-left', {
   },
 });
 
+var swiperHotleThree = new Swiper('.swiper-hotel-left', {
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  mousewheel: {
+    sensitivity: 1,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      centeredSlides: false,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
 var swiperHotleThree = new Swiper('.swiper-hotel-right', {
   slidesPerView: 'auto',
   spaceBetween: 24,
