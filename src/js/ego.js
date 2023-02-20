9d44d77a9de11a963ca675653654c80baf4d44f7
Seven <graf.lancelot7@gmail.com>
2023-02-20 15:40:45 +0200

    slider hotel-three and wow

--- src/js/ego.js
+++ src/js/ego.js
@@ -42,3 +42,26 @@ var swiperAboutLeft = new Swiper('.swiper-about-left', {
     clickable: true,
   },
 });
+
+var swiperHotleThree = new Swiper('.swiper-hotel-right', {
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  mousewheel: {
+    sensitivity: 1,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      centeredSlides: false,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import './js/modal';
 import './js/vik';
 import './js/ego';
 import './js/wow';
+import './js/wow-after';
