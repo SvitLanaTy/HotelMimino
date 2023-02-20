f8f2b4e433a25448b087ab89b3364ed204e1d5da
Seven <graf.lancelot7@gmail.com>
2023-02-20 12:07:40 +0200

    slider main

--- src/js/ego.js
+++ src/js/ego.js
@@ -1,7 +1,31 @@
-var swiper = new Swiper('.swiper-about-right', {
+var swiperAboutRight = new Swiper('.swiper-about-right', {
+  slidesPerView: 'auto',
+  spaceBetween: 25,
+  mousewheel: {
+    sensitivity: 1,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      centeredSlides: false,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+var swiperAboutLeft = new Swiper('.swiper-about-left', {
   slidesPerView: 'auto',
   spaceBetween: 24,
-  centeredSlides: true,
+  mousewheel: {
+    sensitivity: 1,
+  },
   breakpoints: {
     1024: {
       slidesPerView: 2,
